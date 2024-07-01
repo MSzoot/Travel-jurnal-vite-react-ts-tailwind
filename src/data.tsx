243c48a88615d1a4e525c63322160d52c04da26a
MSzoot <97002088+MSzoot@users.noreply.github.com>
2024-07-01 20:42:50 +0100

    up not workinglinks

--- src/data.tsx
+++ src/data.tsx
@@ -1,14 +1,16 @@
-export default [
+const destinations = [
   {
     id: 1,
-    title: 'Mount Fuji',
-    location: 'Japan',
-    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
-    startDate: '12 Jan, 2021',
-    endDate: '24 Jan, 2021',
+    title: 'Mount Everest',
+    location: 'Nepal',
+    googleMapsUrl:
+      'https://www.google.com/maps/place/Everest+Camp+IV+-+South+Col/@27.9883165,86.8813637,12.99z/data=!4m14!1m7!3m6!1s0x39e854a215bd9ebd:0x576dcf806abbab2!2sMt+Everest!8m2!3d27.9881206!4d86.9249751!16zL20vMGJsYmQ!3m5!1s0x39e8549af3ffaeab:0xc692c39524325346!8m2!3d27.9735845!4d86.9299979!16s%2Fg%2F1hhjwbz8d?entry=ttu',
+    startDate: '1 Feb, 2022',
+    endDate: '15 Feb, 2022',
     description:
-      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
-    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+      'Mount Everest is the highest mountain in the world, standing at 8,848 meters (29,029 feet). It is located in the Himalayas on the border between Nepal and Tibet. Climbing Mount Everest is a dream for many adventurers and mountaineers.',
+    imageUrl:
+      'https://images.unsplash.com/3/GoWildImages_MtEverest_NEP0555.jpg?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 2,
@@ -24,13 +26,17 @@ export default [
   },
   {
     id: 3,
-    title: 'Mount Fuji',
-    location: 'Japan',
-    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
-    startDate: '12 Jan, 2021',
-    endDate: '24 Jan, 2021',
+    title: 'Santorini',
+    location: 'Greece',
+    googleMapsUrl:
+      'https://www.google.com/maps/place/Santorini/@36.4070406,25.2558223,11z/data=!3m1!4b1!4m6!3m5!1s0x1499ce86adfd9ff7:0xb2a761f740d68afc!8m2!3d36.3931562!4d25.4615092!16zL20vMDcwdDk?entry=ttu',
+    startDate: '5 Aug, 2021',
+    endDate: '12 Aug, 2021',
     description:
-      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
-    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+      'Santorini is a stunning island in the southern Aegean Sea, known for its dramatic views, stunning sunsets, and white-washed buildings with blue domes. It is a popular destination for honeymooners and travelers seeking beautiful landscapes and rich history.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1678266561093-324802646fb2?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
+
+export default destinations;
